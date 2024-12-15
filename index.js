74e610c8a4dd30fc10ffb2fbba3e59e19a3bb4d4
Sanjay Sanjay <sanjay.05@icloud.com>
2024-12-15 20:28:58 +0530

    added boiler plate

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
+require('dotenv').config();
 
 const app = express()
 
-app.get('/login', () => {
+app.get('/login', (req, res) => {
     console.log('login route')
+    res.send('login')
 })
 
-app.listen(process.env.port, () => {
-    console.log(`Server listening on port ${process.env.port}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`)
 })
